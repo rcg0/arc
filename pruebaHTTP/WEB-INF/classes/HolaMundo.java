fba80672ef1498cf0e8c9a869c7b16650d554ba5
JaviRafa <sanrafja@gmail.com>
2012-10-15 13:53:42 +0200

    servlet de prueba http

---
+++ pruebaHTTP/WEB-INF/classes/HolaMundo.java
@@ -0,0 +1,50 @@
+
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+import java.util.Vector;
+
+import com.google.gson.Gson;
+
+
+public class HolaMundo extends HttpServlet {
+  public void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+
+	PrintWriter out = response.getWriter();
+	String json;
+	Gson gson;
+/*prueba de tablon---> esto habría que hacerlo en estático, claro y luego añadir una lógica que dependiendo del perfil mande unas cosas u otras, esto es 
+unicamente de ejemplo, una prueba de funcionamiento*/
+
+    	Tablon tablon = new Tablon();
+    	tablon.setId("1");
+    	Mensaje mensaje1 = new Mensaje();
+    	mensaje1.setId("1");
+    	mensaje1.setMsg("Mensaje 1");
+    	Mensaje mensaje2 = new Mensaje();
+    	mensaje2.setId("2");
+    	mensaje2.setMsg("Mensaje 2");
+    	tablon.setMsg(mensaje1);
+    	tablon.setMsg(mensaje2);
+/******************/    	
+    	gson = new Gson();
+    	json = gson.toJson(tablon);  
+
+	
+	
+	out.println("parametro: " + json);
+
+  }
+
+  public void doPost(HttpServletRequest request,
+                    HttpServletResponse response)
+      throws ServletException, IOException {
+  
+    doGet(request,response);
+
+    }
+}
+
---
+++ pruebaHTTP/WEB-INF/classes/Mensaje.java
@@ -0,0 +1,31 @@
+
+
+public class Mensaje {
+
+	String id;
+	String msg;
+	
+	public void setId(String id){
+		
+		this.id=id;
+		
+	}
+	
+	public String getId(){
+		
+		return id;
+		
+	}
+	
+	public void setMsg (String msg){
+		
+		this.msg=msg;
+	}
+	
+	public String getMsg(){
+		
+		return msg;
+		
+	}
+	
+}
